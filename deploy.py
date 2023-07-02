3e5846c718830b0e4f6dc270e26c843d10d7e0d7
ZmachinskiiD <98962267+ZmachinskiiD@users.noreply.github.com>
2023-07-02 14:34:56 -0700

    My stuff done

--- deploy.py
+++ deploy.py
@@ -13,53 +13,25 @@ upload_filename = ""
 model_oxford = code_base.getModel() # FROM INFER_ON_SINGLE_IMAGE
 model_paris = code_base.getModel(weights_file="./static/weights/paris_final.pth")
 
-valid_img_oxford = code_base.getQueryNames(labels_dir="./static/data/oxbuild/gt_files/", 
-                img_dir="./static/data/oxbuild/images/") # FROM INFER_ON_SINGLE_IMAGE returns list of images that are grouped
-valid_img_paris = code_base.getQueryNames(labels_dir="./static/data/paris/gt_files/", 
-                img_dir="./static/data/paris/images/") # FROM INFER_ON_SINGLE_IMAGE returns list of images that are grouped
-
 # Main page
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    #return render_template("main.html", valid_img_oxford=valid_img_oxford, valid_img_paris=valid_img_paris) #PEREDAET V TEMPLATE DVA SETA
+    
     return render_template("main.html") 
-
-# Validation page (from main page)
-""" @app.route("/ImgSelected/<group_name>/<img_number>")
-
-def evaluateValid(group_name,img_number): #VIBOR POCHOgICH KARTINOK IZ DATASETOV 
-    if (group_name=="paris"):
-        filename = valid_img_paris[int(img_number)]
-        similar_images, gt = code_base.inference_on_single_labelled_image_pca_web(model_paris,filename,labels_dir="./static/data/paris/gt_files/", 
-                img_dir="./static/data/paris/images/",
-                img_fts_dir="./static/fts_pca/paris/")
-        prev_evaluated_images = similar_images
-        session['prev_evaluated_images'] = prev_evaluated_images
-        return render_template("img_selected.html", filename = filename, evaluated=prev_evaluated_images, gt=gt, valid=1)
-    else:
-        filename = valid_img_oxford[int(img_number)]
-        similar_images, gt = code_base.inference_on_single_labelled_image_pca_web(model_oxford,filename)
-        prev_evaluated_images = similar_images
-        session['prev_evaluated_images'] = prev_evaluated_images
-        return render_template("img_selected.html", filename = filename, evaluated=prev_evaluated_images, gt=gt, valid=1)
- """
-# Validation page repeated (directly from another validation page)
-""" @app.route("/ImgSelected2/<img_number>")
-def evaluateValid2(img_number):
-    filename = session.get('prev_evaluated_images')[int(img_number)]
-    similar_images= code_base.inference_on_single_labelled_image_pca_web_original(model_paris,filename)
-    gt = [0]*60
-    prev_evaluated_images = similar_images
-    session['prev_evaluated_images'] = prev_evaluated_images
-    return render_template("img_selected.html", filename = filename, evaluated=prev_evaluated_images, gt=gt, valid=0) """
 
 # Prediction page
 @app.route("/ImgSelected/upload/", methods=['GET', 'POST'])
 def evaluateNew():
-    if request.method == 'POST' and 'photo' in request.files:
+    if request.method == 'POST' and 'photo'in request.files:
         filename = photos.save(request.files['photo'])
         filename = '/static/temp/'+filename
-        similar_images = code_base.inference_on_single_labelled_image_pca_web_original(model_paris,filename)
+
+        directory_name=request.form.get('directory')
+        directory_name='./static/data/'+request.form.get('directory')+'/'
+        model_name='./static/weights/'+request.form.get('model')+'.pth'
+        model_names=code_base.getModel(weights_file=model_name)
+        print(model_name)
+        similar_images = code_base.inference_on_single_labelled_image_pca_web_original(model_names,filename,directory_name)
         gt = [0]*60
         prev_evaluated_images = similar_images
         session['prev_evaluated_images'] = prev_evaluated_images
--- infer_on_single_image.py
+++ infer_on_single_image.py
@@ -16,78 +16,7 @@ from torch.utils.data import DataLoader
 from inference import get_query_embedding
 
 
-def measure_performance(labels_dir, 
-            img_dir, img_fts_dir,
-            weights_file,
-            subset="inference"):
-    """
-    Given a weights file, calculate the mean average precision over all the queries for the corresponding dataset
-
-    Args:
-        labels_dir  : Directory for ground truth labels
-        img_dir     : Directory holding the images
-        img_fts_dir : Directory holding the pca reduced features generated through create_db.py script
-        weights_file: path of trained weights file
-        subset      : train/ valid/ inference
-    
-    Returns:
-        Mean Average Precision over all queries corresponding to the dataset
-    """
-    # Create Query extractor object
-    QUERY_EXTRACTOR = QueryExtractor(labels_dir, img_dir, subset=subset)
-
-    # Creat image database
-    query_images = QUERY_EXTRACTOR.get_query_names()
-
-    # Create paths
-    query_image_paths = [os.path.join(img_dir, file) for file in query_images]
-
-    aps = []
-    # Now evaluate
-    for i in query_image_paths:
-        ap = inference_on_single_labelled_image_pca(query_img_file=i, labels_dir=labels_dir, img_dir=img_dir, img_fts_dir=img_fts_dir, weights_file=weights_file, plot=False)
-        aps.append(ap)
-
-    
-    return np.array(aps).mean()
-
-
-
-
-
-def getQueryNames(labels_dir="./static/data/oxbuild/gt_files/", 
-                img_dir="./static/data/oxbuild/images/"):
-
-    """
-    Function that returns a list of images that are part of validation set
-
-    Args:
-        labels_dir  : Directory for ground truth labels
-        img_dir     : Directory holding the images
-
-    Returns:
-        List of file paths for images that are part of validation set
-    """
-    QUERY_EXTRACTOR = QueryExtractor(labels_dir, img_dir, subset="inference")
-    query_names=QUERY_EXTRACTOR.get_query_names()
-    for i in range(len(query_names)):
-        query_names[i]=img_dir[1:]+query_names[i]
-    return QUERY_EXTRACTOR.get_query_names()
-
-    
-
 def getModel(weights_file="./static/weights/oxbuild_final.pth"):
-
-    """
-    Function that returns the model (saved during deploy stage to redce load time)
-
-    Args:
-        weights_file: path of trained weights file
-
-    Returns:
-        model based on weights_file
-    """
-
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
     resnet_model = create_embedding_net()
@@ -99,50 +28,23 @@ def getModel(weights_file="./static/weights/oxbuild_final.pth"):
 
 #Были удалены функция для валидации
 ################################################################ NAM NADO OSTSUDA####################################################
-def inference_on_single_labelled_image_pca_web_original(model, query_img_file, 
-                img_dir="./static/data/oxbuild/images/",
-                img_fts_dir="./static/fts_pca/oxbuild/",
-                img_dir2="./static/data/paris/images/",
-                img_fts_dir2="./static/fts_pca/paris/",
-                img_dir3="./static/data/Georgian/",
-                img_fts_dir3="./static/fts_pca/Georgian/",
+def inference_on_single_labelled_image_pca_web_original(model, query_img_file, directory,
                 top_k=60,
                 plot=False,
                 ):
     
-    """
-    Function similar to inference_on_single_labelled_image_pca, but modified return values for usage during web deployment for original images where ground truths are unavailable
-
-    Args:
-        model       : model used (either paris or oxford)
-        query_img_file  : path of query image file
-        img_dir     : Directory holding the images (oxford)
-        img_fts_dir : Directory holding the pca reduced features generated through create_db.py script (oxford)
-        img_dir2    : Directory holding the images (paris)
-        img_fts_dir2: Directory holding the pca reduced features generated through create_db.py script (paris)
-        top_k       : top_k values used to calculate the average precison, default is 60 for web deployment
-        plot        : if True, top 20 results are plotted
-        img_dir3 Georgian
-        img_fts_dir3 georgian
-
-    Returns:
-        List of top k similar images
-    """
-
+   
+    fts_dir=directory+'features'
+    image_dir=directory+'images'
     # Create cuda parameters
     use_cuda = torch.cuda.is_available()
     np.random.seed(2019)
     torch.manual_seed(2019)
     device = torch.device("cuda" if use_cuda else "cpu")
     print("Available device = ", device)
-
     # Creat image database
-    #QUERY_IMAGES_FTS = [os.path.join(img_fts_dir, file) for file in sorted(os.listdir(img_fts_dir))] + [os.path.join(img_fts_dir2, file) for file in sorted(os.listdir(img_fts_dir2))]+ [os.path.join(img_fts_dir3, file) for file in sorted(os.listdir(img_fts_dir3))]
-    QUERY_IMAGES_FTS =  [os.path.join(img_fts_dir3, file) for file in sorted(os.listdir(img_fts_dir3))]
-
-    #QUERY_IMAGES = [os.path.join(img_dir, file) for file in sorted(os.listdir(img_dir))] + [os.path.join(img_dir2, file) for file in sorted(os.listdir(img_dir2))]+ [os.path.join(img_dir3, file) for file in sorted(os.listdir(img_dir3))]
-    QUERY_IMAGES = [os.path.join(img_dir3, file) for file in sorted(os.listdir(img_dir3))]
-    # Query fts
+    QUERY_IMAGES_FTS=[os.path.join(fts_dir, file) for file in sorted(os.listdir(fts_dir))]
+    QUERY_IMAGES= [os.path.join(image_dir, file) for file in sorted(os.listdir(image_dir))]
     query_fts =  get_query_embedding(model, device, "."+query_img_file).detach().cpu().numpy()
     query_fts = perform_pca_on_single_vector(query_fts)
 
@@ -163,7 +65,4 @@ def inference_on_single_labelled_image_pca_web_original(model, query_img_file,
     return best_matches
 
 if __name__ == '__main__':
-    #validate(subset="train")
-    #inference_on_single_labelled_image(query_img_file="./data/oxbuild/images/all_souls_000026.jpg", weights_file="./weights/oxbuild-exp-1.pth")
-    #inference_on_single_labelled_image_pca(query_img_file="./data/oxbuild/images/christ_church_000999.jpg", weights_file="./weights/oxbuild-exp-2.pth")
     pass
